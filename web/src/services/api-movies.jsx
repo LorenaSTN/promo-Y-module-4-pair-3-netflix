c17ecadac20e4a79a53a971954fe3d8496ea3529
LorenaSTN <lorenstn@hotmail.com>
2024-09-09 16:11:12 +0200

    endpoints api-movies

--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,36 +1,43 @@
 // login
 
 const getMoviesFromApi = () => {
-  console.log('Se están pidiendo las películas de la app');
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
-    .then(response => response.json())
-    .then(() => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            genre: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            genre: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
-    });
+  console.log("Se están pidiendo las películas de la app");
+
+  return (
+    fetch("http://localhost:5001/movies")
+      // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
+
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+  );
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
+
+// .then((response) => response.json())
+// .then(() => {
+//   // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
+//   return {
+//     success: true,
+//     movies: [
+//       {
+//         id: "1",
+//         title: "Gambita de dama",
+//         genre: "Drama",
+//         image:
+//           "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg",
+//       },
+//       {
+//         id: "2",
+//         title: "Friends",
+//         genre: "Comedia",
+//         image:
+//           "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg",
+//       },
+//     ],
