52283940c3fd70c71e93ef06dd2cc16b02fcb738
LorenaSTN <lorenstn@hotmail.com>
2024-09-10 15:42:14 +0200

    updating front and back

--- src/index.js
+++ src/index.js
@@ -1,33 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-const fakeMovies = [
-  {
-    id: 1,
-    title: "Wonder Woman",
-    genre: "Action",
-    image:
-      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
-    category: "Superhero",
-    year: 2017,
-    director: "Patty Jenkins",
-  },
-  {
-    id: 2,
-    title: "Inception",
-    genre: "Science Fiction",
-    image:
-      "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-    category: "Thriller",
-    year: 2010,
-    director: "Christopher Nolan",
-  },
-];
+async function getDBConnection() {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "LorenayToby1",
+    database: "netflix",
+  });
+  connection.connect();
+  return connection;
+}
 
 // init express aplication
 const serverPort = 5001;
@@ -35,6 +24,25 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", function (req, res) {
-  res.json(fakeMovies);
+server.get("/movies", async (req, res) => {
+  try {
+    const connection = await getDBConnection();
+    const sqlQuery = "SELECT * FROM netflix.movies";
+    const [result] = await connection.query(sqlQuery);
+
+    connection.end();
+    res.json({
+      status: "success",
+      message: result,
+    });
+  } catch (error) {
+    console.error("Error getting movies:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Error getting movies",
+    });
+  }
 });
+
+// const staticServer = "./web";
+// server.use(express.static(staticServer));
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -6,44 +6,11 @@ async function getMoviesFromApi() {
   const response = await fetch("http://localhost:5001/movies");
   const dataMovies = await response.json();
   console.log(dataMovies);
-
-  /*
-  return (
-    fetch("http://localhost:5001/movies")
-      // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-
-      .then((response) => response.json())
-      .then((data) => {
-        return data;
-      })
-  );
-  */
 }
+getMoviesFromApi();
 
 const objToExport = {
   getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
-
-// .then((response) => response.json())
-// .then(() => {
-//   // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-//   return {
-//     success: true,
-//     movies: [
-//       {
-//         id: "1",
-//         title: "Gambita de dama",
-//         genre: "Drama",
-//         image:
-//           "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg",
-//       },
-//       {
-//         id: "2",
-//         title: "Friends",
-//         genre: "Comedia",
-//         image:
-//           "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg",
-//       },
-//     ],
